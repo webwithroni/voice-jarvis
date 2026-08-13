f202a70df858a5f4d7b8c8e3b0f759a1e9f9a54b
webwithroni <webwithroni@gmail.com>
2026-08-13 11:03:31 +0530

    fix: align Kotlin compiler with Firebase SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("com.google.gms.google-services") version "4.5.0" apply false
     id("com.google.firebase.crashlytics") version "3.0.7" apply false
 }
