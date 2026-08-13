70404103033bdaa619c84c0c9d1a294da1343151
webwithroni <webwithroni@gmail.com>
2026-08-13 11:11:03 +0530

    fix: migrate Kotlin compiler options DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    compilerOptions {
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        )
     }
 
     buildFeatures {
